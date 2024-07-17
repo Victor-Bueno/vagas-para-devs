52ba334a69c338c4253852161fb9d5032824e9f9
Victor Bueno <victor.bueno0720@gmail.com>
2024-07-17 19:14:59 -0300

    chore: Add shadcn/ui and use Inter as default font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
+import './globals.css';
+
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { ReactNode } from 'react';
+
+import { cn } from '@/lib/utils';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,7 +20,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={cn(
+          'bg-background min-h-screen font-sans antialiased',
+          inter.className,
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
