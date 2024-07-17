e1dedbe7e92f8c657348b1fec1e651c52cb8d38a
Victor Bueno <victor.bueno0720@gmail.com>
2024-07-17 18:29:59 -0300

    chore: Add initial project files and configurations

---
+++ README.md
@@ -0,0 +1,1 @@
+# Vagas Para Devs
---
+++ tailwind.config.ts
@@ -0,0 +1,8 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: ['./src/**/*.tsx'],
+  theme: {},
+  plugins: [],
+};
+export default config;
