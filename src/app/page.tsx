9acb7668a2ea58d1f77e05c615e38b9525fa6665
Victor Bueno <victor.bueno0720@gmail.com>
2024-07-18 19:15:09 -0300

    chore: Add filters component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Filters } from '@/components/filters';
 import { Header } from '@/components/header';
 import { Separator } from '@/components/ui/separator';
 
@@ -6,12 +7,12 @@ export default function Home() {
     <>
       <Header />
 
-      <div className="mt-24 flex w-full justify-center">
+      <div className="my-24 flex w-full justify-center">
         <div className="flex max-w-2xl flex-col items-center text-center">
           <h1 className="text-4xl font-semibold">
             Encontre Vagas de <br /> Emprego para Devs
           </h1>
-          <Separator className="bg-primary my-5 h-1 max-w-48" />
+          <Separator className="my-5 h-1 max-w-48 bg-primary" />
           <p className="text-muted-foreground">
             Coleção de vagas de emprego para desenvolvedores de software
             reunidas de diferentes repositórios abastecidos pela comunidade
@@ -19,6 +20,8 @@ export default function Home() {
           </p>
         </div>
       </div>
+
+      <Filters />
     </>
   );
 }
---
+++ src/components/filters.tsx
@@ -0,0 +1,70 @@
+import { Button } from './ui/button';
+import { Checkbox } from './ui/checkbox';
+import { Input } from './ui/input';
+import { Label } from './ui/label';
+import { Popover, PopoverContent, PopoverTrigger } from './ui/popover';
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from './ui/select';
+
+export function Filters() {
+  return (
+    <div className="flex flex-row gap-4">
+      <Input type="text" placeholder="Pesquisar vagas" />
+
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button variant="outline">Filtros</Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-56">
+          <div className="grid gap-4">
+            <h4 className="font-medium leading-none">Área de Atuação</h4>
+            <div className="flex flex-col gap-2">
+              <div className="flex flex-row items-center gap-4">
+                <Checkbox id="backend" />
+                <Label htmlFor="backend">Backend</Label>
+              </div>
+              <div className="flex flex-row items-center gap-4">
+                <Checkbox id="frontend" />
+                <Label htmlFor="frontend">Frontend</Label>
+              </div>
+              <div className="flex flex-row items-center gap-4">
+                <Checkbox id="mobile" />
+                <Label htmlFor="mobile">Mobile</Label>
+              </div>
+              <div className="flex flex-row items-center gap-4">
+                <Checkbox id="data" />
+                <Label htmlFor="data">Dados</Label>
+              </div>
+              <div className="flex flex-row items-center gap-4">
+                <Checkbox id="qa" />
+                <Label htmlFor="qa">QA</Label>
+              </div>
+            </div>
+          </div>
+        </PopoverContent>
+      </Popover>
+
+      <Select defaultValue="relevance">
+        <SelectTrigger className="w-48">
+          <SelectValue placeholder="Ordenar" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectLabel>Ordenar por</SelectLabel>
+            <SelectItem value="relevance">Relevância</SelectItem>
+            <SelectItem value="recent">Mais recente</SelectItem>
+            <SelectItem value="oldest">Mais antigo</SelectItem>
+            <SelectItem value="comments">Mais comentários</SelectItem>
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+    </div>
+  );
+}
