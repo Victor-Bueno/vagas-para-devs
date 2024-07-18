7615a28dd1767cae3aa312a524106470860824ef
Victor Bueno <victor.bueno0720@gmail.com>
2024-07-17 22:42:49 -0300

    chore: Refactor CalloutCircle component to use cn utility function

--- src/components/callout-circle.tsx
+++ src/components/callout-circle.tsx
@@ -1,12 +1,12 @@
 import { ComponentPropsWithoutRef } from 'react';
 
+import { cn } from '@/lib/utils';
+
 interface CalloutCircleProps extends ComponentPropsWithoutRef<'div'> {}
 
 export function CalloutCircle({ className }: CalloutCircleProps) {
   return (
-    <div
-      className={'flex items-center gap-1' + (className ? ` ${className}` : '')}
-    >
+    <div className={cn(className, 'flex items-center gap-1')}>
       <div className="relative size-3.5">
         <div className="bg-primary/30 absolute inset-0 animate-ping rounded-full" />
         <div className="bg-primary/30 absolute inset-0 animate-pulse rounded-full" />
