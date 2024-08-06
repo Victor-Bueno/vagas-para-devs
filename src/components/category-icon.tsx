18d3c446038a73a93baab78501424810b38a059c
Victor Bueno <victor.bueno0720@gmail.com>
2024-08-06 14:10:23 -0300

    refactor: Update category icon component to use a switch statement for determining the category color

--- src/components/category-icon.tsx
+++ src/components/category-icon.tsx
@@ -1,24 +1,30 @@
 import { Brush, Bug, Database, Server, Smartphone } from 'lucide-react';
 
+import { Category } from '@/data/types/job';
 import { cn } from '@/lib/utils';
 
-export type JobCategory = 'frontend' | 'backend' | 'mobile' | 'qa' | 'data';
-
 interface CategoryIconProps {
-  category: JobCategory;
+  category: Category;
 }
 
-const categoryColors = {
-  frontend: 'bg-cyan-600 dark:bg-cyan-500',
-  backend: 'bg-orange-600 dark:bg-orange-500',
-  mobile: 'bg-purple-600 dark:bg-purple-500',
-  qa: 'bg-pink-600 dark:bg-pink-500',
-  data: 'bg-lime-600 dark:bg-lime-500',
+const getCategoryColor = (category: Category) => {
+  switch (category) {
+    case 'frontend':
+      return 'bg-cyan-600 dark:bg-cyan-500';
+    case 'backend':
+      return 'bg-orange-600 dark:bg-orange-500';
+    case 'mobile':
+      return 'bg-purple-600 dark:bg-purple-500';
+    case 'qa':
+      return 'bg-pink-600 dark:bg-pink-500';
+    case 'data':
+      return 'bg-lime-600 dark:bg-lime-500';
+  }
 };
 
 export function CategoryIcon({ category }: CategoryIconProps) {
   return (
-    <div className={cn('rounded-md p-3', categoryColors[category])}>
+    <div className={cn('rounded-md p-3', getCategoryColor(category))}>
       {category === 'frontend' && (
         <Brush
           strokeWidth={1.5}
