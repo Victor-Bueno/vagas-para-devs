5ee59910c2df1d045437f461c56fa6befac99672
Victor Bueno <victor.bueno0720@gmail.com>
2024-08-07 14:32:01 -0300

    refactor: Update job details page layout and add apply button with external link

--- src/app/job/[...data]/page.tsx
+++ src/app/job/[...data]/page.tsx
@@ -1,4 +1,5 @@
 import { ExternalLink } from 'lucide-react';
+import Link from 'next/link';
 
 import { CategoryIcon } from '@/components/category-icon';
 import { JobCarousel } from '@/components/job-carousel';
@@ -39,10 +40,10 @@ export default async function JobPage({
       <div className="grid grid-cols-3 gap-8 pt-16">
         <div className="col-span-2">
           <div className="flex flex-row items-center gap-4">
-            <CategoryIcon category="frontend" />
+            <CategoryIcon category={job.category} />
             <h1 className="text-3xl font-semibold">{job.title}</h1>
           </div>
-          <p className="mt-4 text-muted-foreground">{job.body}</p>
+          <span className="mt-4 text-muted-foreground">{job.body}</span>
         </div>
         <aside className="col-span-1">
           <Card className="sticky top-6">
@@ -55,9 +56,11 @@ export default async function JobPage({
                 <span>Comentários: {job.comments}</span>
                 <span>Postado há: 2 dias atrás</span>
               </CardDescription>
-              <Button className="mt-4 w-full">
-                Candidatar-se <ExternalLink className="ml-2 h-4 w-4" />
-              </Button>
+              <Link href={job.githubUrl} target="_blank">
+                <Button className="mt-4 w-full">
+                  Candidatar-se <ExternalLink className="ml-2 h-4 w-4" />
+                </Button>
+              </Link>
             </CardContent>
           </Card>
         </aside>
